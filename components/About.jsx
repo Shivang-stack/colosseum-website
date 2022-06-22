c341d626f6c5f180c85e82bb0037dbac61c41a69
Shivang-stack <shivang1691401@gmail.com>
2022-06-22 23:27:11 +0530

    changes

--- components/About.jsx
+++ components/About.jsx
@@ -9,30 +9,21 @@ const About = () => {
           <h2 className='py-4'>Whats this fest is all about</h2>
           <p className='py-2 text-gray-300'>Its not your Normal College Fest</p>
           <p className='py-2 text-gray-300'>
-          A WISE MAN ONCE SAID:<br/>
-          “AFTER THE RAIN, THE SUN REAPPEARS.<br/> 
-          THERE IS LIFE. AFTER THE PAIN THE JOY WILL STILL BE HERE.”<br/>
-          And, all of us gathered here, after 2 long gut-wrenching years,<br/>
-          post the pandemic, are living examples, <br/>
-          that we will always rise from everything that happens to us.<br/>
-          COLOSSEUM isnt just a fest, its a legacy in AMCEC that we are <br/>
-          so proud to carry forward.<br/>
-          Let&#39;s GET SET GO for everything that&#39;s coming up!!<br/> 
+          A WISE MAN ONCE SAID:
+          “AFTER THE RAIN, THE SUN REAPPEARS. 
+          THERE IS LIFE. AFTER THE PAIN THE JOY WILL STILL BE HERE.”
+          And, all of us gathered here, after 2 long gut-wrenching years,
+          post the pandemic, are living examples, 
+          that we will always rise from everything that happens to us.
+          COLOSSEUM isnt just a fest, its a legacy in AMCEC that we are 
+          so proud to carry forward.
+          Let&#39;s GET SET GO for everything that&#39;s coming up!! 
           <br/>
           BUCKLE UP YOUR SEATBELTS.
           </p>
-          {/* <p className='py-2 text-gray-300'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting 
-          industry. Lorem Ipsum has been the industrys standard dummy text 
-          ever since the 1500s, when an unknown printer took a galley of 
-          type and scrambled it to make a type specimen book. It has 
-          survived not only five centuries, but also the leap into electronic 
-          typesetting, remaining essentially unchanged. It was popularised 
-          in the 1960s with the release of Letraset sheets containing Lorem 
-          Ipsum passages, and more recently with desktop publishing software 
-          like Aldus PageMaker including versions of Lorem Ipsum
-          
-          </p> */}
+          <p className='py-2 text-gray-300'>
+          Would request you all to show up in large numbers. Get along & make memories with your friends, enjoy the day and also do participate in events & get a chance to win exciting prizes!
+          </p>
           {/* <p className='py-2 text-gray-300 underline cursor-pointer'>Check out some of my latest projects.</p> */}
         </div>
         {/* <div className='w-full h-auto m-auto shadow-xl shadow-gray-800 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
